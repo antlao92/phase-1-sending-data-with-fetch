6e51e46dd2ee3f6b7dddf184b20f217d447c422c
Anthony <anthonykimlao@gmail.com>
2024-03-01 20:21:20 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+const userData = {
+    name: "anthony",
+    email: "anthonykimlao@gmail.com"
+  };
+  
+  // method: "POST" is missing from the object below
+  function submitData(name, email){
+    const userData = {
+        name: name,
+        email: email
+      };
+    const userStringData = JSON.stringify(userData);
+
+   return fetch("http://localhost:3000/users",{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+        body: userStringData
+    })
+    .then(res=>res.json())
+    .then(data=>updateDOM(data))
+    .catch(error=> {
+        let message = "Unauthorized Access"
+        const errorDiv = document.createElement("div");
+        errorDiv.textContent = message;
+        document.body.appendChild(errorDiv);
+      });
+  }
+  
+  submitData("steve", "steve@steve.com")
+
+  function updateDOM(data){ 
+    let newElement = document.createElement('li')
+    newElement.innerHTML = data.id;
+    document.body.appendChild(newElement);
+  }
